357878db33f58c92be135c6b1e1fa69d9e48c85e
Elaine Deng <qd9069@gmail.com>
2022-04-20 00:29:43 -0700

    Added pseudo code

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // TODO: Include packages needed for this application
 
 // TODO: import the generateMarkdown function from utils/generateMarkdown.js
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Write code to get user input, generate markdown, and save it to a file.
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Step1: prompt for following questions
+// 1. title of the project
+// 2. enter description
+// 3. enter installation instructions
+// 4. enter usage information
+// 5. select a license from a list of options
+// 6. enter contribution guidelines
+// 7. enter tests instructions
+// 8. enter for questions: Github username, email address
+
+
+// Step2: to generate the table of contents
+
+// Step3: print all the user responses in the README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,3 +6,18 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+// README list order:
+// 1. title of the project
+// 2. Description
+// 3. Table of Contents 
+  // click on the links here, user will be taken to the corresponding section of README
+// 4. Installation 
+// 5. Usage
+// 6. License: select from an option list
+  // a badge for that license is added near the top of the README
+  // a notice is added to explain which license the application is covered under
+// 7. Contributing
+// 8. Tests
+// 9. Questions: display Github username, with a link to the Github profile
+  // display email address, with instructions on how to reach me with additional questions
