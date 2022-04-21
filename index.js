4d2a32f479d16e24d17398d09f6409a97d565d47
Elaine Deng <qd9069@gmail.com>
2022-04-20 19:34:28 -0700

    Added code for prompting questions in terminal

--- index.js
+++ index.js
@@ -4,19 +4,79 @@
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Write code to get user input, generate markdown, and save it to a file.
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Step1: prompt for following questions
-// 1. title of the project
-// 2. enter description
-// 3. enter installation instructions
-// 4. enter usage information
-// 5. select a license from a list of options
-// 6. enter contribution guidelines
-// 7. enter tests instructions
-// 8. enter for questions: Github username, email address
+inquirer
+  .prompt([
+    // 1. title of the project  
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    // 2. enter description
+    {
+      type: 'input',
+      message: 'Please enter the description of your project.',
+      name: 'description',
+    },
+    // 3. enter installation instructions
+    {
+      type: 'input',
+      message: 'Please enter the installation instructions of your project.',
+      name: 'installation',
+    },
+    // 4. enter usage information
+    {
+      type: 'input',
+      message: 'Please enter the usage information of your project.',
+      name: 'usage',
+    },
+    // 5. select a license from a list of options
+    {
+      type: 'list',
+      message: 'Please choose a license for your project.',
+      name: 'license',
+      choices: ['Apache License 2.0', 
+                'GNU General Public License v3.0', 
+                'MIT License',
+                'Boost Software License 1.0',
+                'GNU Affero General Public License v3.0',
+                'GNU General Public License v2.0',
+                'GNU Lesser General Public License v2.1',
+                'Mozilla Public License 2.0',
+                'The Unlicense'],
+    },
+    // 6. enter contribution guidelines
+    {
+      type: 'input',
+      message: 'Please enter the contribution guidelines of your project.',
+      name: 'contribution',
+    },
+    // 7. enter tests instructions
+    {
+      type: 'input',
+      message: 'Please enter the tests instructions of your project.',
+      name: 'tests',
+    },
+    // 8. enter for questions: Github username, email address
+    {
+      type: 'input',
+      message: 'Please enter your GitBub username.',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your email address.',
+      name: 'email',
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+});
+
 
 
 // Step2: to generate the table of contents
